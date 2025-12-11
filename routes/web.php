5cb88276bacd950f74a400c0f03cec5ae19e27c0
Pisey Tep <piseytep26@gmail.com>
2025-12-11 18:43:05 +0700

    Add debug routes to test API routing on production

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,30 @@ Route::get('/', function () {
         'version' => '1.0.0'
     ]);
 });
+
+// Debug route to test API routing
+Route::post('/api/v1/auth/register-debug', function () {
+    return response()->json([
+        'message' => 'Debug endpoint reached!',
+        'timestamp' => now(),
+        'routes_loaded' => 'yes'
+    ]);
+});
+
+Route::get('/debug-routes', function () {
+    $routes = [];
+    foreach (Route::getRoutes() as $route) {
+        if (strpos($route->uri(), 'api/v1') !== false) {
+            $routes[] = [
+                'uri' => $route->uri(),
+                'methods' => $route->methods(),
+                'name' => $route->getName(),
+                'action' => $route->getActionName()
+            ];
+        }
+    }
+    return response()->json([
+        'api_routes' => $routes,
+        'total_count' => count($routes)
+    ]);
+});
